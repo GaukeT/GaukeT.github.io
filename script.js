80271ff1312a60151a2da7aa80bca0f7a75d4b24
GaukeT <44612327+GaukeT@users.noreply.github.com>
2022-04-26 11:44:30 +0200

    refactor use flexbox (#7)
    clean up
    
    Co-authored-by: GaukeT <gauket.dev@gmail.com>

--- script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('a').click(function(e){
-    if (!$(this).attr('href').startsWith("http")) {
-      e.preventDefault();
-      $("#content").load($(this).attr('href'));
-    }
-  });
-});
