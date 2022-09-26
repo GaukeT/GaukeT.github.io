3941adc135893f275a38007990da5dcff72e6437
GaukeT <gauket.dev@gmail.com>
2022-09-26 15:34:02 +0200

    code cleanup + add new projects

--- README.md
+++ README.md
@@ -1,17 +1,10 @@
 # GaukeT.github.io
-  ### -> [Portfolio](https://gauket.github.io/)
-
-## sub-pages
-  - tea-topics -> [gh-pages](https://gauket.github.io/tea-topics/)
-  - sudoku-solver-js -> [gh-pages](https://gauket.github.io/sudoku-solver-js/)
-  - nonogram-js -> [gh-pages](https://gauket.github.io/nonogram-js/)
-  - sorting-visualization -> [gh-pages](https://gauket.github.io/sorting-visualization/)
-  - flocking-simulation -> [gh-pages](https://gauket.github.io/flocking-simulation/)
-  - seven-segment-clock -> [gh-pages](https://gauket.github.io/seven-segment-clock/)
-  - traffic -> [gh-pages](https://gauket.github.io/traffic/)
+  ### > [Portfolio](https://gauket.github.io/) <
 
 ### deploy
 ```
 ng deploy --base-href=/
 ```
+add CNAME for custom domain
+
 *This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.5.*
--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
@@ -8,18 +8,24 @@ import { Component, OnInit } from '@angular/core';
 export class ProjectsComponent {
 
   projects: project[] = [
-    {name: "Music theory tool", github: "music"},
-    {name: "Tea Topics", github: "tea-topics"},
-    {name: "Sudoku solver", github: "sudoku-solver-js"},
-    {name: "Nonogram", github: "nonogram-js"},
-    {name: "Sorting visualization", github: "sorting-visualization"},
-    {name: "Flocking simulation", github: "flocking-simulation"},
-    {name: "Seven segment clock", github: "seven-segment-clock"},
-    {name: "Traffic", github: "traffic"}
+    {name: "Advent Of Code", github: "adventofcode", demo: true},
+    {name: "Advent Of Code (Java)", github: "adventOfCodeJava", demo: false},
+    {name: "Music", github: "music", demo: true},
+    {name: "Tea Topics", github: "tea-topics", demo: true},
+    {name: "Sudoku solver", github: "sudoku-solver-js", demo: true},
+    {name: "Sudoku solver (C#)", github: "SudokuSolver", demo: false},
+    {name: "Nonogram", github: "nonogram-js", demo: true},
+    {name: "Sorting visualization", github: "sorting-visualization", demo: true},
+    {name: "Flocking simulation", github: "flocking-simulation", demo: true},
+    {name: "Seven segment clock", github: "seven-segment-clock", demo: true},
+    {name: "Breathing exercise", github: "breathing-exercise", demo: true},
+    {name: "Genetic algorithm (Java)", github: "GeneticAlgorithm", demo: false},
+    {name: "Traffic", github: "traffic", demo: true}
   ];
 }
 
 export interface project {
   name: string;
   github: string;
+  demo: boolean;
 }
